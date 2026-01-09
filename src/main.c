172085485e26b0299a06c5294a15fd91e832ff63
Villedieu <Villedieucr@gmail.com>
2026-01-08 19:03:50 -0800

    implemented widget struct and various supporting functions

--- src/main.c
+++ src/main.c
@@ -1,16 +1,103 @@
-#include "gui/draw.h"
-#include "util/err.h"
-#include <stdio.h>
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#define NK_KEYSTATE_BASED_INPUT
 
-int main(void)
-{
-    if (initGui() != 0) {
-        fprintf(stderr, "Failed to initialize GUI\n");
+#define DEFAULT_WINDOW_WIDTH 1200
+#define DEFAULT_WINDOW_HEIGHT 800
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
+
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "nuklear.h"
+#include "nuklear_glfw_gl3.h"
+
+#include "layout/widgets.h"
+
+
+
+static GLFWwindow *window = NULL;
+static struct nk_glfw glfw = {0};
+static struct nk_context *ctx;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void cleanupGui(void);
+void Window_ErrCallback(int error, const char* description);
+
+int main(void){
+    glfwSetErrorCallback(Window_ErrCallback);
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
         return 1;
     }
 
-    drawLoop();
+    /* Create window with OpenGL 3.3 core context */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "JumperCable", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSwapInterval(1); /* Enable vsync */
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return 1;
+    }
+
+    ctx = nk_glfw3_init(&glfw, window, NK_GLFW3_INSTALL_CALLBACKS);
+    struct nk_font_atlas *atlas;
+    nk_glfw3_font_stash_begin(&glfw, &atlas);
+    nk_glfw3_font_stash_end(&glfw);
+
+    initWidgets(ctx);
+
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Input
+        glfwPollEvents();
+        nk_glfw3_new_frame(&glfw);
+
+        drawWidgets();
+
+        /* Render nuklear */
+        nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+        glfwSwapBuffers(window);
+    }
 
     cleanupGui();
-    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void cleanupGui(void){
+    nk_glfw3_shutdown(&glfw);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void Window_ErrCallback(int error, const char* description){
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
--- src/util/err.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ERR_H__
-#define __ERR_H__
-
-void Window_ErrCallback(int error, const char* description);
-
-#endif
